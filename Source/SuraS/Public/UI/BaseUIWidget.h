798c477ff82a553c97124c833466ef33f98d2662
suhyeon Jin <wlstngus1019@gmail.com>
2025-01-18 22:17:50 +0900

    ADD BaseUIWidget

---
+++ Source/SuraS/Public/UI/BaseUIWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseUIWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURAS_API UBaseUIWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
