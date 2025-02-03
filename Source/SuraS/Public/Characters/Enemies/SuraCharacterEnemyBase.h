624021c2884f3e278bc2a22b99993cdf3a349884
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2025-02-03 17:00:29 +0900

    fix: ufunction call for delegate binding functions

--- Source/SuraS/Public/Characters/Enemies/SuraCharacterEnemyBase.h
+++ Source/SuraS/Public/Characters/Enemies/SuraCharacterEnemyBase.h
@@ -26,7 +26,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
 	virtual void OnDamagedTriggered();
+
+	UFUNCTION()
 	virtual void OnDeathTriggered();
 
 public:
