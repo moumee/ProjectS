35edbff5036a9de9de749f162309150c3e6b7537
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2025-02-17 19:50:42 +0900

    update: remove sura base character obsolete attributes ac

--- Source/SuraS/Private/Characters/SuraCharacterBase.cpp
+++ Source/SuraS/Private/Characters/SuraCharacterBase.cpp
@@ -2,17 +2,12 @@
 
 
 #include "Characters/SuraCharacterBase.h"
-#include "ActorComponents/ACAttributes.h"
 
 // Sets default values
 ASuraCharacterBase::ASuraCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	// Attrbutes component
-	AttributesComponent = CreateDefaultSubobject<UACAttributes>(TEXT("AttributesComponent"));
-	AddOwnedComponent(AttributesComponent);
 }
 
 // Called when the game starts or when spawned
