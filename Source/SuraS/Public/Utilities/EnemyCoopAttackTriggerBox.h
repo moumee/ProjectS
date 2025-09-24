eb6112e7f9731d2a763d7cfb3e887b24cc68ce28
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2025-09-25 02:07:56 +0900

    feat: enemy level sequence

--- Source/SuraS/Public/Utilities/EnemyCoopAttackTriggerBox.h
+++ Source/SuraS/Public/Utilities/EnemyCoopAttackTriggerBox.h
@@ -27,8 +27,17 @@ public:
 	TArray<ASuraCharacterEnemyBase*> EnemiesForCoopAttack;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TriggerEnemies")
-	UBoxComponent* TriggerBox;
+	UBoxComponent* EnemiesTriggerBox;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TriggerEnemies")
+	UBoxComponent* PlayerTriggerBox;
 
 	UFUNCTION()
-	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void OnEnemiesOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnPlayerOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnPlayerOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
---
+++ Source/SuraS/Public/Utilities/EnemyLevelSequenceTriggerBox.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemyLevelSequenceTriggerBox.generated.h"
+
+class ULevelSequencePlayer;
+class ULevelSequence;
+class UBoxComponent;
+
+UCLASS()
+class SURAS_API AEnemyLevelSequenceTriggerBox : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEnemyLevelSequenceTriggerBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Trigger LS")
+	UBoxComponent* TriggerBox;
+
+	UPROPERTY(EditAnywhere, Category = "Level Sequence")
+	ULevelSequence* LevelSequenceAsset;
+
+	UPROPERTY()
+	ULevelSequencePlayer* LevelSequencePlayer;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
